01265786880eff5d628276571d8dd9fede9a8ce8
Trang Nguyen <tranguyeenn2007@gmail.com>
2026-06-23 10:38:15 -0400

    Initial Commit

---
+++ ProofIME/ProofIMEApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProofIMEApp.swift
+//  ProofIME
+//
+//  Created by Trang Nguyen on 6/23/26.
+//
+
+import SwiftUI
+
+@main
+struct ProofIMEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
